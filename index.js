21f6c194bc735d0bd743db5bbdbbe7800731508a
Nelson Pacheco <nelsonpacheco@Nelsons-MBP.home>
2021-07-11 11:16:16 +0000

    Getting product reviews

--- index.js
+++ index.js
@@ -25,5 +25,22 @@ app.get('/products/:productId', async (req, res) => {
 });
 
 
+// GET PRODUCT REVIEWS
+
+app.get('/products/:productId/reviews', async (req, res) => {
+    const {productId} = req.params;
+    const {api_key} = req.query;
+
+    try {
+        const response = await request(`${generateScraperUrl(apiKey)}&url=https://www.amazon.com/product-reviews/${productId}`)
+        res.json(JSON.parse(response));
+    } catch (error) {
+        res.json(error);
+    }
+});
+
+
+
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
